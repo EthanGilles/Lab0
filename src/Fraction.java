8afa03e1491e08cd0f4254bcae50f05304c68fc7
EthanGilles <ehgilles01@gmail.com>
2023-09-06 18:27:07 -0400

    Update Fraction.java

--- src/Fraction.java
+++ src/Fraction.java
@@ -3,4 +3,12 @@ public class Fraction implements INumber  {
   int numerator;
   int denominator;
   
-}
+  public Fraction(int n, int d) {
+	  numerator = n;
+	  denominator = d;
+  }
+  
+  
+  
+  
+  	}
