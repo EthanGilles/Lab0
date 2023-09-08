81d21922c30d903df62deac9363378c9a8e4ac31
CobyAndersen10 <143895280+CobyAndersen10@users.noreply.github.com>
2023-09-08 12:48:57 -0400

    Update Fraction.java

--- src/Fraction.java
+++ src/Fraction.java
@@ -62,4 +62,21 @@ public Fraction multiply(Fraction input) {
             input[j + 1] = current;
         }
     }
-  	}
+// Coby Andersen
+public static Fraction findFrequent(Fraction[] input) {
+        Map<Fraction, Integer> frequencyMap = new HashMap<>();
+        Fraction mostFrequent = null;
+        int maxFrequency = 0;
+
+        for (Fraction fraction : input) {
+            int frequency = frequencyMap.getOrDefault(fraction, 0) + 1;
+            frequencyMap.put(fraction, frequency);
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+                mostFrequent = fraction;
+            }
+        }
+
+        return mostFrequent;
+}
+}
