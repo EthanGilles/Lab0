9c8dad4853992a926f7e9f089d8f384ff44837b7
EthanGilles <ehgilles01@gmail.com>
2023-09-12 15:16:45 -0400

    Update Fraction.java

--- src/Fraction.java
+++ src/Fraction.java
@@ -18,7 +18,7 @@ public class Fraction implements INumber<Fraction>  {
  * @return result - no return but prints the fraction.
  */
  public void print() {
-	 System.out.println(this.numerator + "/" + this.denominator);
+	 System.out.print(this.numerator + "/" + this.denominator);
 }
 
 
@@ -27,8 +27,6 @@ public class Fraction implements INumber<Fraction>  {
  * @param input - A fraction object to add to the selected fraction
  * @return result - A simplified fraction object that represents the two fractions added together
  */
- 
- 
 public Fraction plus(Fraction input) {
 	int commonDenominator = lcm(this.denominator, input.denominator);
 	
@@ -51,7 +49,6 @@ public Fraction plus(Fraction input) {
      * @param input The fraction to subtract from this fraction.
      * @return A new fraction representing the result of the subtraction.
      */
-@Override
 public Fraction minus(Fraction input) {
 	int commonDenominator = this.denominator * input.denominator;
         int newNumerator = (this.numerator * input.denominator) - (input.numerator * this.denominator);
@@ -60,7 +57,6 @@ public Fraction minus(Fraction input) {
 
 
 
-@Override
 // Mandy Ho
     public Fraction divide(Fraction input) {
         int newNumerator = this.numerator * input.denominator;
@@ -81,15 +77,14 @@ public Fraction minus(Fraction input) {
  * @return result - a Fraction object equal to the product
  * of the input and the instantiated Fraction.
  */
-
-
-@Override
 public Fraction multiply(Fraction input) {
 	int newNumerator = this.numerator * input.numerator;
 	int newDenominator = this.denominator * input.denominator;
 	Fraction result = new Fraction(newNumerator, newDenominator);
 	return result;
 }
+
+
 
 /**Ethan Gilles
  * Gives the greatest common divisor for two integers
@@ -130,7 +125,7 @@ public static int lcm(int a, int b) {
 
 
 // Mandy Ho	
-    public void insertionSort(Fraction[] input) {
+    public static void insertionSort(Fraction[] input) {
         for (int i = 1; i < input.length; i++) {
             Fraction current = input[i];
             int j = i - 1;
@@ -186,58 +181,163 @@ public static void printReverse(Fraction[] inputArray) {
 	printReverse(newArray);
 }
 
+
 public static void main(String[] args) {
 	
+	
+	//the fraction array described in the lab assignment
 	Fraction[] fractionArray = new Fraction[100];
 		for(int i=0; i<fractionArray.length; i++) {
 			fractionArray[i] = new Fraction(i, i+1);
 		}
 		
+		System.out.println("Outputs show method functionality, these three fractions will be used in the following examples: ");
+		Fraction frac1 = fractionArray[1];
+		Fraction frac2 = fractionArray[4];
+		Fraction frac3 = new Fraction(2,4);
+
 		
+		System.out.print("Fraction 1: ");
+		frac1.print();
+		System.out.println();
+		System.out.print("Fraction 2: ");
+		frac2.print();
+		System.out.println();
+		System.out.print("Fraction 3: ");
+		frac3.print();
+		System.out.println();
 		
+		/*Ethan Gilles
+		 * testing the plus method
+		 * outputs show that the method is functional at adding two fractions
+		 */
+		System.out.println("--- plus Method ---");
 		
+		// addition of fractions using the plus method
+		System.out.print("Fraction 1 + Fraction 2: ");
+		Fraction sum = frac1.plus(frac2);
+		sum.print();
+		System.out.println();
+		System.out.print("Fraction 1 + Fraction 3: ");
+		sum = frac1.plus(frac3);
+		sum.print();
+		System.out.println();
+		/*Ethan Gilles
+		 * testing the minus method
+		 * output shows that the method is functional at subtracting two fractions
+		 */
 		
+		System.out.println("--- minus Method ---");
+		
+		// subtracting of fractions using minus method
+		System.out.print("Fraction 1 - Fraction 2: ");
+		Fraction sum2 = frac1.minus(frac2);
+		sum2.print();
+		System.out.println();
+		System.out.print("Fraction 1 - Fraction 3: ");
+		sum2 = frac1.minus(frac3);
+		sum2.print();
+		System.out.println();
+		
+		/*Ethan Gilles
+		 * testing the multiply method
+		 * outputs show that the method is functional at multiplying two fractions
+		 */
+		System.out.println("--- multiply Method ---");
+		
+		// multiplying two fractions using the multiply method
+		System.out.print("Fraction 1 * Fraction 2: ");
+		Fraction prod = frac1.multiply(frac2);
+		prod.print();
+		System.out.println();
+		System.out.print("Fraction 1 * Fraction 3: ");
+		prod = frac1.multiply(frac3);
+		prod.print();
+		System.out.println();
+		
+		/*Ethan Gilles
+		 * testing the divide method
+		 * outputs show that the method is functional at multiplying two fractions
+		 */
+		System.out.println("--- divideMethod ---");
+		
+		// multiplying two fractions using the multiply method
+		System.out.print("Fraction 1 / Fraction 2: ");
+		Fraction prod2 = frac1.divide(frac2);
+		prod2.print();
+		System.out.println();
+		System.out.print("Fraction 1 / Fraction 3: ");
+		prod2 = frac1.divide(frac3);
+		prod2.print();
+		System.out.println();
 		
 		/*Ethan Gilles
 		 * Testing for hasSameValue method
 		 * Outputs show that the method is functional to show two fractions with the same value
 		 */ 
 		System.out.println("--- hasSameValue Method ---");
-		System.out.print("Fraction 1: ");
-		fractionArray[1].print();
-		System.out.print("Fraction 2: ");
-		fractionArray[4].print();
-		System.out.print("Fraction 3: ");
-		Fraction frac3 = new Fraction(2,4);
-		frac3.print();
-		
 		
 		// comparisons of fractions using the hasSameValue method 
-		System.out.println("Fraction 1 compared to Fraction 2: " + fractionArray[1].hasSameValue(fractionArray[4]));
-		System.out.println("Fraction 1 compared to Fraction 3: " + fractionArray[1].hasSameValue(frac3));
-		
+		System.out.println("Fraction 1 compared to Fraction 2: " + frac1.hasSameValue(frac2));
+		System.out.println("Fraction 1 compared to Fraction 3: " + frac1.hasSameValue(frac3));
 		
 		
 		/*Ethan Gilles
-		 * testing for plus method
-		 * outputs show that the method is functional at adding two methods
-		 */
-		System.out.println("--- plus Method ---");
+		 * Testing for findFrequent method
+		 * Outputs show that the method is functional to find the most frequent fraction in an array
+		 */ 
+		System.out.println("--- findFrequent Method ---");
 		
-		System.out.print("Fraction 1: ");
-		fractionArray[1].print();
-		System.out.print("Fraction 2: ");
-		fractionArray[4].print();
-		System.out.print("Fraction 3: ");
-		frac3.print();
+		//calling the find frequent method to find the most common fraction in the array.
+		Fraction mostFrequent = findFrequent(fractionArray);
 		
-		// addition of fractions using the plus method
-		System.out.print("Fraction 1 + Fraction 2: ");
-		Fraction sum = fractionArray[1].plus(fractionArray[4]);
-		sum.print();
-		System.out.print("Fraction 1 + Fraction 3: ");
-		sum = fractionArray[1].plus(frac3);
-		sum.print();
+		System.out.print("The most frequent fraction was: ");
+		mostFrequent.print();
+		System.out.println();
+		
+		/*Ethan Gilles
+		 * Testing for insertionSort method
+		 * Outputs show that the method is functional to sort the array using the insertion sort algorithm
+		 */ 
+		System.out.println("--- insertionSort Method ---");
+		
+		//shows the array before the sort
+		System.out.print("before: [");
+		for (Fraction frac : fractionArray) {
+			frac.print();
+			System.out.print(", ");
+		}
+		System.out.println("]");
+		
+		insertionSort(fractionArray);
+		
+		//shows the array after the sort
+		System.out.print("after:  [");
+		for (Fraction frac : fractionArray) {
+			frac.print();
+			System.out.print(", ");
+		}
+		System.out.println("]");
+		
+		/*Ethan Gilles
+		 * Testing for printReverse method
+		 * Outputs show that the method is functional to print a fraction array in reverse.
+		 */ 
+		System.out.println("--- printReverse Method ---");
+		
+		
+		//shows the array before printing in reverse
+		System.out.print("before: [");
+		for (Fraction frac : fractionArray) {
+			frac.print();
+			System.out.print(", ");
+		}
+		System.out.println("]");
+		
+		
+		System.out.print("after:  ");
+		printReverse(fractionArray);
+		
 }
 
 }
